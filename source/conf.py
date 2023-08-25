a72027e925c7e0a7270fa24c0b938fa65abfac04
BryceDixon <bryce.dixon.bd@gmail.com>
2023-08-25 15:17:17 +0100

    gettingstarted

--- source/conf.py
+++ source/conf.py
@@ -36,7 +36,7 @@ extensions = ["sphinx.ext.autodoc",
               "sphinx.ext.doctest",
               "sphinx.ext.viewcode",
               "sphinx.ext.mathjax",
-              "nbsphinx"]
+              "nbsphinx", "myst_nb"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
