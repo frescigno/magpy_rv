1d312eb1f416dcdc2933c26fc95d25a14c58ab93
BryceDixon <bryce.dixon.bd@gmail.com>
2023-08-29 12:04:30 +0100

    math test

--- src/magpy_rv/kernels.py
+++ src/magpy_rv/kernels.py
@@ -120,9 +120,10 @@ class Cosine(Kernel):
     '''Class that computes the Cosine kernel matrix.
     
     Kernel formula:
+    
     .. math::
         
-        K = H_1^2 \cos(\frac{2\pi \cdot |t-t'|}{H_2})
+        K = H_1^2 \\cos(\\frac{2\\pi \\cdot |t-t'|}{H_2})
     
     in which:
         H_1 = variance/amp
