a5bd97e4b8c1e1e9b651a899556de951e7b3809e
BryceDixon <bryce.dixon.bd@gmail.com>
2023-08-29 12:20:12 +0100

    math test

--- src/magpy_rv/kernels.py
+++ src/magpy_rv/kernels.py
@@ -126,12 +126,9 @@ class Cosine(Kernel):
         K = H_1^2 \\cos(\\frac{2\\pi \\cdot |t-t'|}{H_2})
     
     in which:
-    
-    .. math::
-    
-        H_1 = \\frac{variance}{amp}
+        :math:`H_1 = \\frac{variance}{amp}`
         
-        H_2 = per
+        :math:`H_2 = per`
         
     Arguments:
         hparams: dictionary with all the hyperparameters.
