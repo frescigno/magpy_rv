451a849bc228e6b983b5484681def40cc37a1019
BryceDixon <bryce.dixon.bd@gmail.com>
2023-08-29 12:15:58 +0100

    math test

--- src/magpy_rv/kernels.py
+++ src/magpy_rv/kernels.py
@@ -126,12 +126,15 @@ class Cosine(Kernel):
         K = H_1^2 \\cos(\\frac{2\\pi \\cdot |t-t'|}{H_2})
     
     in which:
-        :math: `H_1 = \\frac{variance}{amp}`
+    
+    .. math::
+    
+        H_1 = \\frac{variance}{amp}
         
-        :math: `H_2 = per`
+        H_2 = per
         
     Arguments:
-        hparams: dictionary with all the hyperparameters,
+        hparams: dictionary with all the hyperparameters.
             Should have 2 elements with possibly errors
     '''
     
