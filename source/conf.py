59f22a016c13d7febddc0aeef0be3a6848a3cbaf
Federica Rescigno <fr307@exeter.ac.uk>
2023-09-13 15:16:21 +0100

    update version

--- source/conf.py
+++ source/conf.py
@@ -23,7 +23,7 @@ copyright = '2023, Federica Rescigno'
 author = 'Federica Rescigno'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '1.0.4'
 
 
 # -- General configuration ---------------------------------------------------
